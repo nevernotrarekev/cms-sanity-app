efc7ebc3f79d059d65c60f9a1363d6cb5434b8f0
huckpilot <huckpilot@gmail.com>
2020-11-10 12:46:43 -0800

    Add slick styles to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import "../styles/index.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick.scss";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
