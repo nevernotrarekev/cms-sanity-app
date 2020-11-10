16602fedc2d47ae2356a1c214fc8348969fe412b
huckpilot <huckpilot@gmail.com>
2020-11-10 08:57:44 -0800

    Remove post css updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,4 @@
-const path = require("path");
-
 module.exports = {
-  sassOptions: {
-    includePaths: [
-      path.join(__dirname, "styles"),
-      path.join(__dirname, "node_modules/slick-carousel/slick/"),
-    ],
-  },
   plugins: [
     "tailwindcss",
     ...(process.env.NODE_ENV === "production"
