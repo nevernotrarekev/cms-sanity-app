834a162c23d91c2253d1c42bf14e25ff455edd0a
huckpilot <huckpilot@gmail.com>
2020-11-23 08:03:22 -0800

    Burger menu

--- components/header.js
+++ components/header.js
@@ -1,11 +1,33 @@
+import react, { useState, useEffect } from "react";
 import Link from "next/link";
 import LogoSvg from "./logo-svg";
-import styles from "./header.module.css";
+import styles from "./header.module.scss";
+import cn from "classnames";
+import { useRouter } from "next/router";
 
 export default function Header() {
+  const router = useRouter();
+  const [burgerOpen, setBurgerOpen] = useState(true);
   const classes = {
+    links: `${styles.links} ${burgerOpen ? "open" : ""}`,
     link: `ml-12 ${styles.link}`,
+    burgerWrap: `${styles.burgerWrap} ${burgerOpen ? "open" : ""}`,
   };
+
+  useEffect(() => {
+    const handleRouteComplete = (url) => {
+      console.log("App changed to: ", url);
+      setBurgerOpen(false);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteComplete);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method:
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteComplete);
+    };
+  }, []);
   return (
     <nav className="container max-w-wide mx-auto px-5 flex lg:flex-row justify-between items-center lg:mb-12 md:mb-6 mb-20 mt-8">
       <div className="w-168px">
@@ -15,7 +37,14 @@ export default function Header() {
           </a>
         </Link>
       </div>
-      <div className="ml-5">
+      <div
+        className={cn(styles.burgerWrap, burgerOpen ? styles.open : null)}
+        onClick={() => setBurgerOpen(!burgerOpen)}
+      >
+        <div></div>
+        <div></div>
+      </div>
+      <div className={cn(styles.links, burgerOpen ? styles.open : null)}>
         <Link href={"/work"}>
           <a className={classes.link}>Work</a>
         </Link>
