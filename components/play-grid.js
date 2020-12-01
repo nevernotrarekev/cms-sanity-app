5269ab7031f6be22c92022187cd7d65693547837
huckpilot <huckpilot@gmail.com>
2020-11-30 21:02:12 -0800

    Update platy with masonry

--- components/play-grid.js
+++ components/play-grid.js
@@ -2,6 +2,7 @@ import Vimeo from "@u-wave/react-vimeo";
 import { imageBuilder } from "../lib/sanity";
 import styles from "./play-grid.module.scss";
 import cn from "classnames";
+import Masonry from "react-masonry-css";
 
 const PlayGrid = ({ items }) => {
   const classes = {
@@ -9,15 +10,20 @@ const PlayGrid = ({ items }) => {
   };
   return (
     <>
-      <div className="grid grid-cols-12 gap-8">
+      {/* <div className="grid grid-cols-12 gap-8"> */}
+      <Masonry
+        breakpointCols={3}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
         {items.length > 0 &&
           items.map((item, index) => {
             return (
               <div
                 className={cn(
                   styles.playGridItem,
-                  item.vimeoid ? "col-span-8" : "col-span-4",
-                  index === 8 && "col-start-9 col-span-4"
+                  item.vimeoid ? "col-span-8" : "col-span-4"
+                  // index === 8 && "col-start-9 col-span-4"
                 )}
               >
                 {item.playImage && (
@@ -34,7 +40,8 @@ const PlayGrid = ({ items }) => {
               </div>
             );
           })}
-      </div>
+      </Masonry>
+      {/* </div> */}
     </>
   );
 };
