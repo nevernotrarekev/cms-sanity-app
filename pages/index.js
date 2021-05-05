3b94457fc02760497cbac18364150e316ecda880
nevernotrarekev <kevin@rareart.io>
2021-05-05 12:48:31 -0400

    Added Metatags

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,25 @@ export default function Index({ allPosts, homeData, preview }) {
       <Layout preview={preview}>
         <Head>
         <title>{CMS_NAME} | Creative Collective </title>
+        <meta name="description" content="A Creative Collective Focused on Production, Post-Production, and Design">
+
+                <!-- Facebook Meta Tags -->
+                <meta property="og:url" content="https://mndl.com">
+                <meta property="og:type" content="website">
+                <meta property="og:title" content="Mondial | Creative Collective ">
+                <meta property="og:description" content="A Creative Collective Focused on Production, Post-Production, and Design">
+                <meta property="og:image" content="https://mondial.vercel.app/footer-illo.png">
+
+                <!-- Twitter Meta Tags -->
+                <meta name="twitter:card" content="summary_large_image">
+                <meta property="twitter:domain" content="mndl.com">
+                <meta property="twitter:url" content="https://mndl.com">
+                <meta name="twitter:title" content="Mondial | Creative Collective ">
+                <meta name="twitter:description" content="A Creative Collective Focused on Production, Post-Production, and Design">
+                <meta name="twitter:image" content="https://mondial.vercel.app/footer-illo.png">
+
+                <!-- Meta Tags Generated via https://www.opengraph.xyz -->
+                </meta>
         </Head>
         <Container>
           <Intro text={homeData.text} image={homeData.illo} />
