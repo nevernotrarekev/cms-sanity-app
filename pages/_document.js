4b422abf4ef903b0a9893bf002b359a0caad1bf4
christian-hopin <christian@hopin.to>
2021-05-14 13:06:54 -0400

    image

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,12 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="stylesheet" href="https://use.typekit.net/vsk5xim.css" />
+          <meta
+            property="og:image"
+            content={
+              "https://user-images.githubusercontent.com/972100/118293617-d28e2080-b4a7-11eb-93ad-631d8019cd80.jpg"
+            }
+          />
         </Head>
         <body>
           <Main />
